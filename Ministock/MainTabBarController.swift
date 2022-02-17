23cfefe1dcd9e0a098410152e3dbc3157322fd5e
DoChangHwan <88352580+DoChangHwan@users.noreply.github.com>
2022-02-17 16:45:36 +0900

    Create MainTabBarController.swift

---
+++ Ministock/MainTabBarController.swift
@@ -0,0 +1,19 @@
+//
+//  MainTabBarController.swift
+//  Ministock
+//
+//  Created by dochanghwan on 2022/02/17.
+//
+
+import UIKit
+
+
+class MainTabBarController: UITabBarController {
+    // Todo: UIViewController 4개
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        viewControllers = [UIViewController(), UIViewController()]
+        
+    }
+    
+}
