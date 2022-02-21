e216d90669b1d738dd6cc3bcaefeef62b6f3b6f7
DoChangHwan <88352580+DoChangHwan@users.noreply.github.com>
2022-02-21 23:43:21 +0900

    Update HomeViewController.swift

--- Ministock/HomeViewController.swift
+++ Ministock/HomeViewController.swift
@@ -15,15 +15,8 @@ struct CustomData {
  // 홈 화면
 class HomeViewController: UIViewController, UICollectionViewDelegate {
     
-    private lazy var pageControl: UIPageControl = {
-        let pageControl = UIPageControl()
-        pageControl.numberOfPages = 5
-        pageControl.backgroundColor = .systemGray2
-        return pageControl
-    }()
+    private lazy var scrollMenuView = UIScrollView()
     
-    private lazy var scrollView = UIScrollView()
-
     private lazy var scrollHomeView: UIScrollView = {
         let scrollView = UIScrollView()
         scrollView.backgroundColor = .systemBackground
@@ -31,7 +24,7 @@ class HomeViewController: UIViewController, UICollectionViewDelegate {
         return scrollView
     }()
 
-    private lazy var buttonTwo: UIButton = {
+    private lazy var buttonHomeBottom: UIButton = {
         let button = UIButton()
         button.setTitle("더보기", for: .normal)
         button.setTitleColor(.black, for: .normal)
@@ -40,66 +33,14 @@ class HomeViewController: UIViewController, UICollectionViewDelegate {
         button.translatesAutoresizingMaskIntoConstraints = false
         return button
     }()
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        view.backgroundColor = .systemBackground
-        
-        addHomeSubView()
-        setupHomeAutolayout()
-    }
-    
-    func addHomeSubView() {
-        self.view.addSubview(scrollHomeView)
-        self.view.addSubview(collectionTopimageView)
-        view.addSubview(pageControl)
-        view.addSubview(scrollView)
-        scrollHomeView.addSubview(buttonTwo)
-    }
-    
-    override func viewDidLayoutSubviews() {
-        super.viewDidLayoutSubviews()
-        pageControl.frame = CGRect(x: 10, y: view.frame.size.height-140, width: view.frame.size.width-20, height: 30)
-        scrollView.frame = CGRect(x: 0, y: 250, width: view.frame.size.width, height: view.frame.size.height/2)
-        
-        if scrollView.subviews.count == 2 {
-            configureScrollView()
-        }
-    }
-
-    private func setupHomeAutolayout() {
-        scrollHomeView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20).isActive = true
-        scrollHomeView.topAnchor.constraint(equalTo: view.topAnchor, constant: 20).isActive = true
-        scrollHomeView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -20).isActive = true
-        scrollHomeView.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: -20).isActive = true
-
-        buttonTwo.centerXAnchor.constraint(equalTo: view.safeAreaLayoutGuide.centerXAnchor).isActive = true
-        buttonTwo.widthAnchor.constraint(equalTo: view.widthAnchor, multiplier: 0.7).isActive = true
-        buttonTwo.topAnchor.constraint(equalTo: scrollHomeView.topAnchor, constant: 1000).isActive = true
-        buttonTwo.bottomAnchor.constraint(equalTo: scrollHomeView.bottomAnchor, constant: -40).isActive = true
-        
-        collectionTopimageView.topAnchor.constraint(equalTo: view.topAnchor, constant: 16).isActive = true
-        collectionTopimageView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 16).isActive = true
-        collectionTopimageView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -16).isActive = true
-        collectionTopimageView.heightAnchor.constraint(equalTo: collectionTopimageView.widthAnchor, multiplier: 0.7).isActive = true
-        
-        collectionTopimageView.delegate = self
-        collectionTopimageView.dataSource = self
-        
-        pageControl.addTarget(self, action: #selector(pageControlDidChange(_:)), for: .valueChanged) // 클릭시 전환
-        
-        scrollView.delegate = self
-
-    }
-    
-// title 배너 collectionview 구현
+   // title 배너 collectionview 구현
     let data = [
         CustomData(title: "first", image: #imageLiteral(resourceName: "first")),
         CustomData(title: "second", image: #imageLiteral(resourceName: "second")),
         CustomData(title: "third", image: #imageLiteral(resourceName: "third")),
     ]
-    
-    fileprivate let collectionTopimageView: UICollectionView = {
+        
+    private lazy var collectionTopimageView: UICollectionView = {
         let layout = UICollectionViewFlowLayout()
         layout.scrollDirection = .horizontal
         let cv = UICollectionView(frame: .zero, collectionViewLayout: layout)
@@ -108,17 +49,75 @@ class HomeViewController: UIViewController, UICollectionViewDelegate {
         cv.register(CustomCell.self, forCellWithReuseIdentifier: "cell")
         return cv
     }()
+    
+    // page 표시
+    private lazy var pageControl: UIPageControl = {
+        let pageControl = UIPageControl()
+        pageControl.numberOfPages = 3
+        pageControl.backgroundColor = .systemCyan
+        return pageControl
+    }()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemBackground
+        addHomeSubView()
+        setupHomeAutolayout()
+    }
+    
+    func addHomeSubView() {
+        view.addSubview(scrollHomeView)
+        view.addSubview(pageControl)
+        scrollHomeView.addSubview(collectionTopimageView)
+        scrollHomeView.addSubview(scrollMenuView)
+        scrollHomeView.addSubview(buttonHomeBottom)
+    }
+
+    private func setupHomeAutolayout() {
+        scrollHomeView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor).isActive = true
+        scrollHomeView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor).isActive = true
+        scrollHomeView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor).isActive = true
+        scrollHomeView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor).isActive = true
+
+        buttonHomeBottom.centerXAnchor.constraint(equalTo: scrollHomeView.safeAreaLayoutGuide.centerXAnchor).isActive = true
+        buttonHomeBottom.widthAnchor.constraint(equalTo: scrollHomeView.widthAnchor, multiplier: 0.7).isActive = true
+        buttonHomeBottom.topAnchor.constraint(equalTo: scrollHomeView.topAnchor, constant: 1000).isActive = true
+        buttonHomeBottom.bottomAnchor.constraint(equalTo: scrollHomeView.bottomAnchor, constant: -60).isActive = true
+
+        collectionTopimageView.topAnchor.constraint(equalTo: scrollHomeView.topAnchor).isActive = true
+        collectionTopimageView.leadingAnchor.constraint(equalTo: scrollHomeView.leadingAnchor).isActive = true
+        collectionTopimageView.trailingAnchor.constraint(equalTo: scrollHomeView.trailingAnchor).isActive = true
+        collectionTopimageView.widthAnchor.constraint(equalTo: scrollHomeView.widthAnchor).isActive = true
+        collectionTopimageView.heightAnchor.constraint(equalTo: collectionTopimageView.widthAnchor, multiplier: 0.7).isActive = true
+    
+        collectionTopimageView.delegate = self
+        collectionTopimageView.dataSource = self
+        
+        pageControl.addTarget(self, action: #selector(pageControlDidChange(_:)), for: .valueChanged) // 클릭시 전환
+        scrollMenuView.delegate = self
+
+    }
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        pageControl.frame = CGRect(x: 20, y: collectionTopimageView.frame.size.height+50, width: collectionTopimageView.frame.size.width/3, height: 30)
+        scrollMenuView.frame = CGRect(x: 0, y: 250, width: view.frame.size.width, height: view.frame.size.height/2)
+        
+        if scrollMenuView.subviews.count == 2 {
+            configureScrollMenuView()
+        }
+    }
 
 // 클릭시 전환
     @objc private func pageControlDidChange(_ sender: UIPageControl) {
         let current = sender.currentPage
-        scrollView.setContentOffset(CGPoint(x: CGFloat(current) * view.frame.size.width, y: 0), animated: true)
+        collectionTopimageView.setContentOffset(CGPoint(x: CGFloat(current) * view.frame.size.width, y: 0), animated: true)
     }
     
 // 스크롤시 화면 이동
-    private func configureScrollView() {
-        scrollView.contentSize = CGSize(width: view.frame.size.width*5, height: scrollView.frame.size.height)
-        scrollView.isPagingEnabled = true
+    private func configureScrollMenuView() {
+        scrollMenuView.contentSize = CGSize(width: view.frame.size.width*5, height: scrollMenuView.frame.size.height)
+        scrollMenuView.isPagingEnabled = true
         let colors: [UIColor] = [
         .systemRed,
         .systemGray,
@@ -127,21 +126,22 @@ class HomeViewController: UIViewController, UICollectionViewDelegate {
         .systemPurple
         ]
         for x in 0..<5 {
-            let page = UIView(frame: CGRect(x: CGFloat(x)*view.frame.size.width, y: 0, width: view.frame.size.width, height: scrollView.frame.size.height))
+            let page = UIView(frame: CGRect(x: CGFloat(x)*view.frame.size.width, y: 0, width: view.frame.size.width, height: view.frame.size.height))
             page.backgroundColor = colors[x]
-            scrollView.addSubview(page)
+            scrollMenuView.addSubview(page)
         }
     }
 
 }
 
-// 스크롤시 바 상태 변화
+// title 배너 스크롤시 바 상태 변화
 extension HomeViewController: UIScrollViewDelegate {
     func scrollViewDidScroll(_ scrollView: UIScrollView) {
-        pageControl.currentPage = Int(floorf(Float(scrollView.contentOffset.x) / Float(scrollView.frame.width)))
+        pageControl.currentPage = Int(floorf(Float(collectionTopimageView.contentOffset.x) / Float(collectionTopimageView.frame.width)))
     }
 }
 
+// title 배너 설정
 extension HomeViewController: UICollectionViewDelegateFlowLayout, UICollectionViewDataSource {
         func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
             return CGSize(width: collectionView.frame.width, height: collectionView.frame.width/2)
@@ -187,5 +187,37 @@ class CustomCell: UICollectionViewCell {
     }
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
+    }
+}
+
+class MenuBarScrollView: UIScrollView {
+    
+    private lazy var stackView: UIStackView = {
+        let sv = UIStackView()
+        sv.spacing = 20
+        return sv
+    }()
+    
+    private lazy var barView: UIView = {
+        let view = UIView()
+        view.backgroundColor = .red
+        return view
+    }()
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setupStackViews()
+        setupStackLayout()
+    }
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    private func setupStackViews() {
+        addSubview(stackView)
+        addSubview(barView)
+    }
+    private func setupStackLayout() {
+        stackView.topAnchor.constraint(equalTo: superview.topAnchor)
+        
     }
 }
