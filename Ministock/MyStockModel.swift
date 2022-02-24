32009ed9e609e785a91e09b16497a22132ad027c
DoChangHwan <88352580+DoChangHwan@users.noreply.github.com>
2022-02-24 16:48:16 +0900

    Create MyStockModel.swift

---
+++ Ministock/MyStockModel.swift
@@ -0,0 +1,19 @@
+//
+//  MyStockModel.swift
+//  Ministock
+//
+//  Created by dochanghwan on 2022/02/24.
+//
+
+import Foundation
+
+
+struct MyStockModel: Decodable {
+    let stockName: String
+    let currentPrice: Int
+    let stockQuantity: Float
+    let valueChange: Int
+    let percentChange: Float
+    let imageURL: String
+    
+}
